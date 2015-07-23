e2c173e204b517ec85affd0ef3f25f20600fb562
koriyama <koriyama@mfpc205.local>
2015-07-23 19:17:18 +0900

    全部scriptに載せる

--- src/main.js
+++ src/main.js
@@ -2,4 +2,7 @@
 
 let css = require('./style.less');
 
+let comoponent1_template = require('./components/components1/components1.jade');
+let comoponent1_script = require('./components/components1/components1.js');
+
 console.log('*** mock ***');
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   entry: "./src/main.js",
   output: {
-    path: "./build",
-    filename: "script.js"
+    filename: "./public/[name].js"
   },
   module: {
     loaders: [
